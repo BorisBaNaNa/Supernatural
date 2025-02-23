3cc6be78674492870ea6f1f19780a1209f9b9048
BorisBaNaNa <hahaha7733489@gmail.com>
2025-02-23 18:25:36 +0300

    separated player logic

---
+++ Assets/Supernatural/Scripts/Player/Player.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.Windows;
+
+namespace Assets.Supernatural.Scripts.Player
+{
+    [RequireComponent(typeof(Controller2D), typeof(AudioSource))]
+    public partial class Player : MonoBehaviour, ICanTakeDamage
+    {
+        #region Inspector
+        public string CurrentState;
+        public bool GodMode;
+
+        [SerializeField] private PlayerMovementController _movementController;
+
+        [Header("Health")]
+        public int maxHealth;
+        public int Health { get; set; }
+        public GameObject HurtEffect;
+
+        [Header("Sound")]
+        public AudioClip jumpSound;
+        public AudioClip landSound;
+        public AudioClip WalkSound;
+        public AudioClip wallSlideSound;
+        public AudioClip hurtSound;
+        public AudioClip deadSound;
+        public AudioClip rangeAttackSound;
+        public AudioClip meleeAttackSound;
+
+        [Header("Option")]
+        public bool allowMeleeAttack;
+        public bool allowRangeAttack;
+        [Range(0.01f, 1)]
+        public float MinActiveStateTime = 0.1f;
+        #endregion
+
+        public PlayerWeapon CurrentWeapon { get; set; } = PlayerWeapon.None;
+        public AudioSource SoundFx { get; private set; }
+
+        private PlayerStateMachine _stateMachine;
+        private InputActions _inputs;
+
+        public void Awake()
+        {
+            Initialize();
+        }
+
+        public void Start()
+        {
+            FillingFields();
+
+            _inputs.Player.RangeAttack.performed += _ => RangeAttack();
+            _inputs.Player.MeleeAttack.performed += _ => MeleeAttack();
+        }
+
+        public void Update()
+        {
+            _movementController.MoveLogic();
+        }
+
+        #region Init
+        private void Initialize()
+        {
+            _inputs = new InputActions();
+            SoundFx = GetComponent<AudioSource>();
+        }
+
+        private void FillingFields()
+        {
+            Health = maxHealth;
+            SoundFx.clip = wallSlideSound;
+        }
+        #endregion
+
+        #region API
+
+        public void MeleeAttack()
+        {
+            if (allowMeleeAttack)
+                _stateMachine.StateSwitch<MelleAttackState>();
+        }
+
+        public void RangeAttack()
+        {
+            if (allowRangeAttack)
+                _stateMachine.StateSwitch<RangeAttackState>();
+        }
+
+        public void RespawnAt(Vector2 pos)
+        {
+            transform.position = pos;
+
+            _stateMachine.StateSwitch<RespawnState>();
+        }
+
+        public void TakeDamage(float damage, Vector2 forceDir, GameObject instigator)
+        {
+            if (CurrentState == "DeathState")
+                return;
+
+            _stateMachine.StateSwitch<TakeDamageState>();
+
+            if (GodMode)
+                return;
+
+            Health -= (int)damage;
+
+            if (Health <= 0)
+                Kill();
+
+            if (forceDir.x == 0 && forceDir.y == 0)
+                return;
+
+            _movementController.SetDamageImpulse(instigator.transform);
+        }
+
+        public void GiveHealth(int hearthToGive, GameObject instigator)
+        {
+            Health = Mathf.Min(Health + hearthToGive, maxHealth);
+            //GameManager.Instance.ShowFloatingText("+" + hearthToGive, transform.position, Color.red);
+        }
+
+        public void Kill() =>
+            _stateMachine.StateSwitch<DeathState>();
+
+        public void GameFinish() =>
+            _stateMachine.StateSwitch<FinishState>();
+        #endregion
+    }
+}
---
+++ Assets/Supernatural/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+
+namespace Assets.Supernatural.Scripts.Player
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        public bool IsSliding
+        {
+            get
+            {
+                Vector2 dir = new(controller.collisions.left ? -1 : 1, 0);
+                RaycastHit2D hit = Physics2D.Raycast(SlidePoint.position, dir, 0.6f, LayerMask.GetMask("Ground"));
+
+                bool nearWall = controller.collisions.left || controller.collisions.right;
+                bool isMoveDown = !controller.collisions.below && velocity.y < 0;
+                return allowSlideWall && hit && nearWall && isMoveDown;
+            }
+        }
+
+        [Header("Animations")]
+        [SerializeField] private PlayerAnimController AnimController;
+
+        [Header("Moving")]
+        [SerializeField] private float moveSpeed = 3;
+
+        [Header("Jump")]
+        [SerializeField] private float JumpDelay = 0.1f;
+        [SerializeField] private float MaxJumpHeight = 3;
+        [SerializeField] private float MinJumpHeight = 1;
+        [SerializeField] private float TimeToJumpApex = .4f;
+        [SerializeField] private int NumberOfJumpMax = 1;
+        [SerializeField] private GameObject JumpEffect;
+
+        [Header("Wall SlideFaceToWall")]
+        [SerializeField] private Transform SlidePoint;
+        [SerializeField] private Vector2 wallJumpClimb;
+        [SerializeField] private Vector2 wallJumpOff;
+        [SerializeField] private Vector2 wallLeap;
+        [SerializeField] private float wallSlideSpeedMax = 3;
+        [SerializeField] private float wallStickTime = .25f;
+
+        public float MinVelosityForLand = 3;
+        public bool allowSlideWall;
+
+
+        public int WallDirX => controller.collisions.left ? -1 : controller.collisions.right ? 1 : 0;
+        public bool WasGrounded => _wasGrounded;
+        public bool IsGrounded => controller.collisions.below;
+        public bool IsHardLand => _isHardLand;
+
+        private float velocityXSmoothing;
+        private float _gravity;
+        private bool _wasGrounded;
+        private bool _isHardLand;
+
+        private Vector2 velocity;
+        private Vector2 _moveDir;
+
+        private float accelerationTimeAirborne = .2f;
+        private float accelerationTimeGrounded = .1f;
+        private Controller2D controller;
+        private InputActions _inputs;
+        private PlayerStateMachine _stateMachine;
+
+        public void Awake()
+        {
+            Initialize();
+        }
+
+        public void Start()
+        {
+            SubscribeInputs();
+        }
+
+        public void OnEnable()
+        {
+            _inputs.Player.Move.Enable();
+        }
+
+        public void OnDisable()
+        {
+            _inputs.Player.Move.Disable();
+        }
+
+        private void Initialize()
+        {
+            _inputs = new InputActions();
+
+            //_stateMachine = new(this);
+            _stateMachine.StateSwitch<IdleState>();
+        }
+
+        private void SubscribeInputs()
+        {
+            _inputs.Player.Jump.performed += _ => Jump();
+            _inputs.Player.Jump.canceled += _ => JumpOff();
+        }
+
+        public void MoveLogic()
+        {
+#if !UNITY_ANDROID
+            HandleInput();
+#endif
+            ConfigureMove();
+
+            CalculateVelocity();
+            CheckLand();
+
+            _stateMachine.CurrentAction?.Invoke();
+            Move();
+            _stateMachine.StateControl();
+        }
+
+        public void SetDamageImpulse(Transform instigator)
+        {
+            var facingDirectionX = Mathf.Sign(transform.position.x - instigator.position.x);
+            var facingDirectionY = Mathf.Sign(velocity.y);
+
+            SetForce(new Vector2(
+                Mathf.Clamp(Mathf.Abs(velocity.x), 10, 15) * facingDirectionX,
+                Mathf.Clamp(Mathf.Abs(velocity.y), 5, 15) * -facingDirectionY)
+            );
+        }
+
+        public void SetForce(Vector2 force)
+        {
+            velocity = (Vector3)force;
+        }
+
+        public void AddForce(Vector2 force)
+        {
+            velocity += force;
+        }
+
+        public void Jump()
+        {
+            controller.IsJumpKeyPressed = true;
+            if (_moveDir.y >= 0)
+                _stateMachine.StateSwitch<JumpState>();
+        }
+
+        public void JumpOff()
+        {
+            controller.IsJumpKeyPressed = false;
+            AllServices.Instance.GetService<JumpState>().JumpOff();
+        }
+
+        public void Flip() =>
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
+        private void CalculateVelocity()
+        {
+            velocity.x = ApplySmoothing();
+            velocity.y += _gravity * Time.deltaTime;
+        }
+
+#if !UNITY_ANDROID
+        private void HandleInput() =>
+            _moveDir = _inputs.Player.Move.ReadValue<Vector2>();
+#endif
+
+        private void ConfigureMove()
+        {
+            if (_moveDir.x != 0 && Mathf.Sign(transform.localScale.x) != _moveDir.x)
+                Flip();
+
+            if (_moveDir.magnitude > 1f)
+                _moveDir.Normalize();
+        }
+
+        private float ApplySmoothing()
+        {
+            float targetVelocityX = _moveDir.x * moveSpeed;
+            float smoothTime = (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne;
+            return Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, smoothTime);
+        }
+
+        private void CheckLand()
+        {
+            _isHardLand = Mathf.Abs(velocity.y) > MinVelosityForLand;
+            _wasGrounded = IsGrounded;
+        }
+
+        private void Move()
+        {
+            controller.Move(velocity * Time.deltaTime, _moveDir);
+            if (controller.collisions.above || IsGrounded)
+                velocity.y = 0;
+        }
+    }
+}
